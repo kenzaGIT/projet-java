ba9c5be72dc2fb269109298dd1893ca6ecc77fea
k <bouhafskenza@hotmail.fr>
2023-12-02 13:45:28 +0100

    java qqsdqd

--- src/projet/Projet.java
+++ src/projet/Projet.java
@@ -16,7 +16,9 @@ public class Projet {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("hjedqfqsdf");
-        System.out.println("projet.Projet.main()");
+
+        System.out.print("qsdqsdyiazesqd");
+
     }
     
 }
